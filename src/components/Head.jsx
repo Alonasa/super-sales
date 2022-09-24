6b2df7de8b7214c89c4fb5657efbe32468cef7c0
Alonesa <alenakrasnov@mail.ru>
2022-09-24 21:43:24 +0300

    added mui icons

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {PrimarySearchAppBar} from "./Menu";
 
 export const Head = () => {
   return (
     <section>
+      <PrimarySearchAppBar/>
       <div>
         <h3>Super Sales</h3>
       </div>

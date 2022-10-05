42a112fa1fbd231eb9bb2dbd9d8be3c64c19503d
Alonesa <alenakrasnov@mail.ru>
2022-10-05 21:13:21 +0300

    added container to menu

--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -14,7 +14,7 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import {Favorite} from '@mui/icons-material';
 import AddIcon from '@mui/icons-material/Add';
-import {Button} from '@mui/material';
+import {Button, Container} from '@mui/material';
 
 
 export const AppMenu = () => {
@@ -134,73 +134,75 @@ export const AppMenu = () => {
   return (
     <Box sx={{flexGrow: 1}}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{mr: 2}}
-          >
-            <MenuIcon/>
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{display: {xs:'block'}}}
-          >
-            Super Sales
-          </Typography>
-          <Box sx={{flexGrow: 1}}/>
-          <Box sx={{display: {xs: 'none', md: 'flex'}}}>
-            <IconButton size="large" aria-label="show 4 new mails"
-                        color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon/>
-              </Badge>
-            </IconButton>
-            <IconButton size="large" aria-label="show products from wishlist"
-                        color="inherit">
-              <Badge badgeContent={10} color="secondary">
-                <Favorite/>
-              </Badge>
-            </IconButton>
+        <Container>
+          <Toolbar>
             <IconButton
               size="large"
-              aria-label="show 17 new notifications"
+              edge="start"
               color="inherit"
+              aria-label="open drawer"
+              sx={{mr: 2}}
             >
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon/>
-              </Badge>
+              <MenuIcon/>
             </IconButton>
-            <Button style={{color: "#fff"}}>Post Add</Button>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{display: {xs:'block'}}}
             >
-              <AccountCircle/>
-            </IconButton>
-          </Box>
-          <Box sx={{display: {xs: 'flex', md: 'none'}}}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon/>
-            </IconButton>
-          </Box>
-        </Toolbar>
+              Super Sales
+            </Typography>
+            <Box sx={{flexGrow: 1}}/>
+            <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+              <IconButton size="large" aria-label="show 4 new mails"
+                          color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                  <MailIcon/>
+                </Badge>
+              </IconButton>
+              <IconButton size="large" aria-label="show products from wishlist"
+                          color="inherit">
+                <Badge badgeContent={10} color="secondary">
+                  <Favorite/>
+                </Badge>
+              </IconButton>
+              <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+              >
+                <Badge badgeContent={17} color="secondary">
+                  <NotificationsIcon/>
+                </Badge>
+              </IconButton>
+              <Button style={{color: "#fff"}}>Post Add</Button>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle/>
+              </IconButton>
+            </Box>
+            <Box sx={{display: {xs: 'flex', md: 'none'}}}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon/>
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
       </AppBar>
       {renderMobileMenu}
       {renderMenu}

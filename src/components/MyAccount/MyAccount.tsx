ad9a5fd907ca2571689c2dbff0595f1b085e6cfe
Alonesa <alenakrasnov@mail.ru>
2022-10-18 19:28:31 +0300

    added empty  menu pages

---
+++ src/components/MyAccount/MyAccount.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MyAccount = () => {
+  return (
+	<div>
+	  This is account page
+	</div>
+  );
+};
---
+++ src/components/PostAdd/PostAdd.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const PostAdd = () => {
+  return (
+	<div>
+	  This is popsting page
+	</div>
+  );
+};

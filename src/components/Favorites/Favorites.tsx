9088f4a0036f9e2b8f6c3a4a3ef299683015e79b
Alonesa <alenakrasnov@mail.ru>
2022-10-21 00:18:17 +0300

    make favorite page

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Add, AddsType} from '../Ads/Add';
+import {Container} from '@mui/material';
 
 export const Favorites = (props: AddsType) => {
   return (
-	<Add  items={props.items.filter(i => i.isFavorite)}/>
+	<Container style={{padding: '2vh 5vw'}}>
+	  <Add items={props.items.filter(i => i.isFavorite)}/>
+	</Container>
   );
 };

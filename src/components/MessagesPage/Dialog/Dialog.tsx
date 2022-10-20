bcd68512b1f9b982b333c3c7d9fd93197891fa6a
Alonesa <alenakrasnov@mail.ru>
2022-10-20 21:48:54 +0300

    make a simple dialog page

--- src/components/MessagesPage/Dialog/Dialog.tsx
+++ src/components/MessagesPage/Dialog/Dialog.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styles from './Dialog.module.css'
 
 export const Dialog = () => {
   return (
-	<div>
-	  
+	<div className={styles.dialog}>
+	  <img className={styles.dialog__avatar}
+		   src={'https://cdn4.iconfinder.com/data/icons/must-have-outline/100/objects-29-1024.png'}/>
+	  <div className={styles.dialog__info}>
+		<span className={styles.dialog__title}>My Name</span>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+		  consequuntur debitis dolor, ea eligendi eos, eum harum hic impedit, iure
+		  nesciunt quaerat ratione ut veniam voluptatem! Eum laudantium nihil
+		  vero.</p>
+	  </div>
 	</div>
   );
 };
--- src/components/MessagesPage/Messages/Messages.tsx
+++ src/components/MessagesPage/Messages/Messages.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Dialog} from '../Dialog/Dialog';
+import {Container} from '@mui/material';
 
 export const Messages = () => {
   return (
-	<div>
-	  Messages
-	</div>
+	<Container style={{padding: '0 5vw'}}>
+	  <Dialog/>
+	</Container>
   );
 };

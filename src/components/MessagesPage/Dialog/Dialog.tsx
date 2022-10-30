be51c69322299977759ea4ca8ad8005390bbbcba
Alonesa <alenakrasnov@mail.ru>
2022-10-30 23:00:22 +0200

    added basic messages

--- src/components/MessagesPage/Dialog/Dialog.tsx
+++ src/components/MessagesPage/Dialog/Dialog.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import styles from './Dialog.module.css'
+import styles from './Dialog.module.css';
 
-export const Dialog = () => {
+type DialogsType = {
+  id: string
+  firstName: string
+  secondName: string
+  isOnline: boolean
+  messages: Array<string>
+}
+
+export type DialogsDataType = {
+  data: Array<DialogsType>
+}
+
+export const Dialog = (props: DialogsDataType) => {
   return (
-	<div className={styles.dialog}>
-	  <img className={styles.dialog__avatar} alt={'this is my avatar'}
-		   src={'https://cdn4.iconfinder.com/data/icons/must-have-outline/100/objects-29-1024.png'}/>
-	  <div className={styles.dialog__info}>
-		<span className={styles.dialog__title}>My Name</span>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-		  consequuntur debitis dolor, ea eligendi eos, eum harum hic impedit, iure
-		  nesciunt quaerat ratione ut veniam voluptatem! Eum laudantium nihil
-		  vero.</p>
-	  </div>
-	</div>
+	<>
+	  {props.data.map(d => {
+		return (
+		  <div key={d.id} className={styles.dialog}>
+			<img className={styles.dialog__avatar} alt={'this is my avatar'}
+				 src={'https://cdn4.iconfinder.com/data/icons/must-have-outline/100/objects-29-1024.png'}/>
+			<div className={styles.dialog__info}>
+			<span
+			  className={styles.dialog__title}>{d.firstName} {d.secondName}</span>
+			  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+				consequuntur debitis dolor, ea eligendi eos, eum harum hic
+				impedit,
+				iure
+				nesciunt quaerat ratione ut veniam voluptatem! Eum laudantium
+				nihil
+				vero.</p>
+			</div>
+		  </div>
+		)
+	  })}
+	</>
   );
 };
--- src/components/MessagesPage/Messages/Messages.tsx
+++ src/components/MessagesPage/Messages/Messages.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import {Dialog} from '../Dialog/Dialog';
 import {Container} from '@mui/material';
+import {v1} from 'uuid';
+
+const dialogsData = [
+  {
+	id: v1(),
+	firstName: 'Bob',
+	secondName: 'Michael',
+	isOnline: false,
+	messages: ['Hi', 'What are you doing?', 'Will you join lesson today?']
+  },
+  {
+	id: v1(),
+	firstName: 'Anna',
+	secondName: 'Michael',
+	isOnline: true,
+	messages: ['Hi', 'What are you doing?', 'Will you join lesson today?']
+  },
+  {
+	id: v1(),
+	firstName: 'Kate',
+	secondName: 'Michael',
+	isOnline: false,
+	messages: ['Hi', 'What are you doing?', 'Will you join lesson today?']
+  },
+]
 
 export const Messages = () => {
   return (
 	<Container style={{padding: '0 5vw'}}>
-	  <Dialog/>
+	  <Dialog data={dialogsData}/>
 	</Container>
   );
 };

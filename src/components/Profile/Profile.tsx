15b21d9fbeb19181801740258c364964b7010909
Alonesa <alenakrasnov@mail.ru>
2022-11-03 22:47:57 +0200

    installed hash link added some text to it

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import s from './Profile.module.css'
 import {Button, Container} from '@mui/material';
-import {FavoriteBorder, FavoriteOutlined} from '@mui/icons-material';
-
+import {FavoriteBorder} from '@mui/icons-material';
+import {HashLink} from 'react-router-hash-link';
 
 export const Profile = () => {
   return (
@@ -19,16 +19,239 @@ export const Profile = () => {
 		  </ul>
 		</div>
 		<div className={s.profile__contact_data}>
-		  <Button size={'small'} variant={'outlined'}><FavoriteBorder/> Follow</Button>
+		  <Button size={'small'}
+				  variant={'outlined'}><FavoriteBorder/> Follow</Button>
 		  <Button size={'small'} variant={'contained'}>Show phone</Button>
 		</div>
 	  </div>
 	  <div className={s.profile__content}>
-		<div className={s.profile__navigation_buttons}>Nav buttons</div>
+		<div className={s.profile__navigation_buttons}>
+		  <HashLink to="/profile#all-ads">All Ads</HashLink>
+		  <HashLink to="/profile#about">About Us</HashLink>
+		  <HashLink to="/profile#contact">Contact Info</HashLink>
+		</div>
 		<div className={s.profile__data}>
-		  <div>Ads</div>
-		  <div>User description</div>
-		  <div>Contact Info</div>
+		  <div id="all-ads">Ads
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add Add
+			Add Add Add Add Add Add Add Add Add Add Add
+		  </div>
+		  <div id="about">
+			About UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout UsAbout
+			UsAbout UsAbout UsAbout UsAbout Us
+		  </div>
+		  <div id="contact">
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info Heres Our Contact Info Heres Our Contact Info
+			Heres Our Contact Info
+		  </div>
 		</div>
 	  </div>
 	  

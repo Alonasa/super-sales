8f6ae486c712525f32b3e3817751ad4d176f5952
Alonesa <alenakrasnov@mail.ru>
2022-11-20 20:32:23 +0200

    added types for messagesContent connect

--- src/components/MessagesPage/MessagesContent/MessagesContentContainer.tsx
+++ src/components/MessagesPage/MessagesContent/MessagesContentContainer.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {MessagesContent} from './MessagesContent';
 import {connect} from 'react-redux';
+import {AppType} from '../../../App';
 
-let mapStateToProps = (state: any) => {
+let mapStateToProps = (state: AppType) => {
   return {
 	state: state
   }
 }
 
-let mapDispatchToProps = (dispatch: any) => {
+let mapDispatchToProps = (dispatch: (dispatch: {type: string, newMessage: string})=>void) => {
   return {
-    dispatch: (newMessage: string)=> {
-      dispatch({type: 'ADD-MESSAGE', newMessage: newMessage.trim()})
+	dispatch: (newMessage: string) => {
+	  dispatch({type: 'ADD-MESSAGE', newMessage: newMessage.trim()})
 	}
   }
 }
